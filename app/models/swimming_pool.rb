ed10e8ecb98dbdd02f178aa08bcdfb99c4ccbd13
Suzanna Filipowska <suzanna.filipowska@gmail.com>
2020-08-18 12:24:10 +0200

    refactored validations accrdingly

--- app/models/swimming_pool.rb
+++ app/models/swimming_pool.rb
@@ -1,7 +1,5 @@
 class SwimmingPool < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  validates :capacity, presence: true
-  validates :location, presence: true
-  validates :size, presence: true
+  validates :capacity, :location, :size, presence: true
 end
