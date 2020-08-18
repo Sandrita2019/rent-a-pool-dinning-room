6f4f7f5e04d94a2f10740582d13fc4b90936f5a1
Beatriz Longley <beatrizsousacosta@gmail.com>
2020-08-18 19:57:44 +0200

    Created the actions show, new, create / build the structure for view pages for both

--- app/controllers/swimming_pools_controller.rb
+++ app/controllers/swimming_pools_controller.rb
@@ -3,5 +3,25 @@ class SwimmingPoolsController < ApplicationController
   end
 
   def show
+    @swimming_pool = Swimming_pool.find(params[:id])
+  end
+
+  def new
+    @swimming_pool = Swimming_pool.new
+  end
+
+  def create
+    @swimming_pool = Swimming_pool.new(swimming_pool_params)
+    if @swimming_pool.save
+      redirect_to swimming_pool_path(@swimming_pool)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def swimming_pool_params
+    params.require(:swimming_pool).permit(:capacity, :location, :size)
   end
 end
