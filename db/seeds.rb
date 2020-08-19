f38f3c8a1638ba0b44d5655b75ef14f408e1814a
Beatriz Longley <beatrizsousacosta@gmail.com>
2020-08-19 12:26:02 +0200

    Populate the database with users and swimming pools

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+puts 'Creating swimming pools....'
+
+6.times do
+  swimming_pool = SwimmingPool.new(
+    title: Faker::FunnyName.two_word_name,
+    description: Faker::ChuckNorris.fact,
+    size: rand(10..100),
+    location: Faker::Address.street_address,
+    capacity: rand(2..10)
+  )
+  user = User.create!(
+    email: Faker::Internet.email,
+    password: "123456"
+    )
+  swimming_pool.user = user
+  swimming_pool.save!
+end
+puts 'Finished!'
