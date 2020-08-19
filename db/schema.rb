0dad3c982e118ccdebed247df3e86271ab9ce95d
Sandra Porqueres <sandraporqueres@gmail.com>
2020-08-19 11:35:47 +0200

    migrattion message and number_people  booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_140239) do
+ActiveRecord::Schema.define(version: 2020_08_19_092940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2020_08_18_140239) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "status"
+    t.string "message"
+    t.integer "number_people"
     t.index ["swimming_pool_id"], name: "index_bookings_on_swimming_pool_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
