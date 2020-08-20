9d06e840f100e7f001c6b8e3bba883d7e6b9e204
Sandra Porqueres <sandraporqueres@gmail.com>
2020-08-20 16:48:56 +0200

    html part of the bookings page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "clean database"
+Booking.delete_all
+SwimmingPool.delete_all
+
 require 'faker'
 puts 'Creating swimming pools....'
 
