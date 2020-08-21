eedaf092a9cb9f75d069db403521b440aeda1cd2
Sandra Porqueres <sandraporqueres@gmail.com>
2020-08-21 16:02:16 +0200

    changes end controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,2 @@
 class PagesController < ApplicationController
-  # skip_before_action :authenticate_user!, only: :home
-  # skip_before_action :authenticate_user # test
-  # skip_before_filter :verify_authenticity_token
-  # skip_before_filter :authenticate_user!, :raise => false
-  end
+end
